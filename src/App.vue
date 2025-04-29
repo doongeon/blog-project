a05513873b150b62d472dad07087474c959b1282
donggeon Na <ndmb2012@gmail.com>
2025-04-29 14:03:21 +0900

    프로젝트 초기화

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <NavView />
+  <RouterView />
+  <FooterView />
+</template>
+
+<script setup lang="ts">
+import NavView from '@/components/NavView.vue';
+import { RouterView } from 'vue-router';
+import FooterView from './components/FooterView.vue';
+</script>
---
+++ src/router/index.ts
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import TheTest from '@/views/TheTest.vue';
+import PostsView from '@/views/PostsView.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/posts',
+      name: 'posts',
+      component: PostsView,
+    },
+    {
+      path: '/posts/:id',
+      name: '/post',
+      component: PostsView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: TheTest,
+    },
+    {
+      path: '/posts/write',
+      name: '/posts/write',
+      component: HomeView,
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue'),
+    // },
+  ],
+});
+
+export default router;
